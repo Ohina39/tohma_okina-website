b66e2bc3f1bc0132bf0019420ff2fa7fb497d8ad
haruhide39 <sc91rdkpsng3dbuneeew@docomo.ne.jp>
2024-10-21 23:01:22 +0900

    first

---
+++ main.js
@@ -0,0 +1,7 @@
+const drawCanvas = () => {
+    const canvas = document.getElementById("canvas");
+    canvas.innerHTML = "在庫切れです";
+    canvas.style.color = '#00FF00';
+    canvas.style.backgroundColor = 'red'; // 背景色変更
+    canvas.style.padding = '10px'; // 領域を広くする
+}
