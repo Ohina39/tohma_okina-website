0e6be9b35062060ae8f318e4fe8467c09ee30c73
haruhide39 <sc91rdkpsng3dbuneeew@docomo.ne.jp>
2024-11-13 19:51:04 +0900

    ポップアップ表示完成

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-const drawCanvas = () => {
-    const canvas = document.getElementById("canvas");
-    canvas.innerHTML = "在庫切れです";
-    canvas.style.color = '#00FF00';
-    canvas.style.backgroundColor = 'red'; // 背景色変更
-    canvas.style.padding = '10px'; // 領域を広くする
-    console.log("a");
-}
 
 const myfunc1 = () => {
 
@@ -14,31 +6,349 @@ const myfunc1 = () => {
 
 
 
+function OnLinkClick1() {
+    var Popup1 = document.getElementById("PopupFrame1");
+    var Top_mozi = document.getElementById("PopupLink1");
 
-function togglePopup1() {
-    var popup = document.getElementById("imgPopup1");
-    if (popup.style.display === 'none') {
-        popup.style.display = 'block';
+    var Popup2 = document.getElementById("PopupFrame2");
+    var News_mozi = document.getElementById("PopupLink2");
+
+    var Popup3 = document.getElementById("PopupFrame3");
+    var Profile_mozi = document.getElementById("PopupLink3");
+
+    var Popup4 = document.getElementById("PopupFrame4");
+    var People_mozi = document.getElementById("PopupLink4");
+
+    var Popup5 = document.getElementById("PopupFrame5");
+    var Music_mozi = document.getElementById("PopupLink5");
+
+    var Popup6 = document.getElementById("PopupFrame6");
+    var Blog_mozi = document.getElementById("PopupLink6");
+
+    Top_mozi.style.color = '#a0d3f7';
+    
+    if (Popup1.style.visibility === "hidden") {
+        Popup1.style.visibility = "visible";
+
+        Top_mozi.style.color = '#e3eb9b';
+        Top_mozi.style.pointerEvents = "none";
+
+        Popup2.style.visibility = "hidden";
+        News_mozi.style.pointerEvents = "auto";
+        News_mozi.style.color = '#ffffff';
+
+        Popup3.style.visibility = "hidden";
+        Profile_mozi.style.pointerEvents = "auto";
+        Profile_mozi.style.color = '#ffffff';
+
+        Popup4.style.visibility = "hidden";
+        People_mozi.style.pointerEvents = "auto";
+        People_mozi.style.color = '#ffffff';
+
+        Popup5.style.visibility = "hidden";
+        Music_mozi.style.pointerEvents = "auto";
+        Music_mozi.style.color = '#ffffff';
+
+        Popup6.style.visibility = "hidden";
+        Blog_mozi.style.pointerEvents = "auto";
+        Blog_mozi.style.color = '#ffffff';
+
     } else {
-        popup.style.display = 'none';
+        Popup1.style.visibility = "hidden";
+        Top_mozi.style.color = '#ffffff';
     }
-  }
 
-
-
-function OnLinkClick() {
-    var linkelem = document.getElementById("PopupLink1");
-    var clientRect = linkelem.getBoundingClientRect();
-
-    var elem = document.getElementById("PopupFrame1");
-    elem.style.visibility = "visible"
-
-    elem.style.left = "300px";
-    elem.style.top = "10px";;
+    Popup1.style.left = "300px";
+    Popup1.style.top = "10px";
+    
 }
 
-function OnCloseLinkClick() {
-    var elem = document.getElementById("PopupFrame1");
-    elem.style.visibility = "hidden";
+function OnLinkClick2() {
+    var Popup1 = document.getElementById("PopupFrame1");
+    var Top_mozi = document.getElementById("PopupLink1");
+
+    var Popup2 = document.getElementById("PopupFrame2");
+    var News_mozi = document.getElementById("PopupLink2");
+
+    var Popup3 = document.getElementById("PopupFrame3");
+    var Profile_mozi = document.getElementById("PopupLink3");
+
+    var Popup4 = document.getElementById("PopupFrame4");
+    var People_mozi = document.getElementById("PopupLink4");
+
+    var Popup5 = document.getElementById("PopupFrame5");
+    var Music_mozi = document.getElementById("PopupLink5");
+
+    var Popup6 = document.getElementById("PopupFrame6");
+    var Blog_mozi = document.getElementById("PopupLink6");
+
+    News_mozi.style.color = '#a0d3f7';
+    
+    if (Popup2.style.visibility === "hidden") {
+        Popup2.style.visibility = "visible";
+
+        Popup1.style.visibility = "hidden";
+        Top_mozi.style.pointerEvents = "auto";
+        Top_mozi.style.color = '#ffffff';
+
+        News_mozi.style.color = '#e3eb9b';
+        News_mozi.style.pointerEvents = "none";
+
+        Popup3.style.visibility = "hidden";
+        Profile_mozi.style.pointerEvents = "auto";
+        Profile_mozi.style.color = '#ffffff';
+
+        Popup4.style.visibility = "hidden";
+        People_mozi.style.pointerEvents = "auto";
+        People_mozi.style.color = '#ffffff';
+
+        Popup5.style.visibility = "hidden";
+        Music_mozi.style.pointerEvents = "auto";
+        Music_mozi.style.color = '#ffffff';
+
+        Popup6.style.visibility = "hidden";
+        Blog_mozi.style.pointerEvents = "auto";
+        Blog_mozi.style.color = '#ffffff';
+
+    } else {
+        Popup2.style.visibility = "hidden";
+        News_mozi.style.color = '#ffffff';
+    }
+
+    Popup2.style.left = "300px";
+    Popup2.style.top = "10px";
+    
 }
 
+
+function OnLinkClick3() {
+    var Popup1 = document.getElementById("PopupFrame1");
+    var Top_mozi = document.getElementById("PopupLink1");
+
+    var Popup2 = document.getElementById("PopupFrame2");
+    var News_mozi = document.getElementById("PopupLink2");
+
+    var Popup3 = document.getElementById("PopupFrame3");
+    var Profile_mozi = document.getElementById("PopupLink3");
+
+    var Popup4 = document.getElementById("PopupFrame4");
+    var People_mozi = document.getElementById("PopupLink4");
+
+    var Popup5 = document.getElementById("PopupFrame5");
+    var Music_mozi = document.getElementById("PopupLink5");
+
+    var Popup6 = document.getElementById("PopupFrame6");
+    var Blog_mozi = document.getElementById("PopupLink6");
+
+    Profile_mozi.style.color = '#a0d3f7';
+    
+    if (Popup3.style.visibility === "hidden") {
+        Popup3.style.visibility = "visible";
+
+        Popup1.style.visibility = "hidden";
+        Top_mozi.style.pointerEvents = "auto";
+        Top_mozi.style.color = '#ffffff';
+
+        Popup2.style.visibility = "hidden";
+        News_mozi.style.pointerEvents = "auto";
+        News_mozi.style.color = '#ffffff';
+
+        Profile_mozi.style.color = '#e3eb9b';
+        Profile_mozi.style.pointerEvents = "none";
+
+        Popup4.style.visibility = "hidden";
+        People_mozi.style.pointerEvents = "auto";
+        People_mozi.style.color = '#ffffff';
+
+        Popup5.style.visibility = "hidden";
+        Music_mozi.style.pointerEvents = "auto";
+        Music_mozi.style.color = '#ffffff';
+
+        Popup6.style.visibility = "hidden";
+        Blog_mozi.style.pointerEvents = "auto";
+        Blog_mozi.style.color = '#ffffff';
+
+    } else {
+        Popup3.style.visibility = "hidden";
+        Profile_mozi.style.color = '#ffffff';
+    }
+
+    Popup3.style.left = "300px";
+    Popup3.style.top = "10px";
+    
+}
+
+
+function OnLinkClick4() {
+    var Popup1 = document.getElementById("PopupFrame1");
+    var Top_mozi = document.getElementById("PopupLink1");
+
+    var Popup2 = document.getElementById("PopupFrame2");
+    var News_mozi = document.getElementById("PopupLink2");
+
+    var Popup3 = document.getElementById("PopupFrame3");
+    var Profile_mozi = document.getElementById("PopupLink3");
+
+    var Popup4 = document.getElementById("PopupFrame4");
+    var People_mozi = document.getElementById("PopupLink4");
+
+    var Popup5 = document.getElementById("PopupFrame5");
+    var Music_mozi = document.getElementById("PopupLink5");
+
+    var Popup6 = document.getElementById("PopupFrame6");
+    var Blog_mozi = document.getElementById("PopupLink6");
+
+    People_mozi.style.color = '#a0d3f7';
+    
+    if (Popup4.style.visibility === "hidden") {
+        Popup4.style.visibility = "visible";
+
+        Popup1.style.visibility = "hidden";
+        Top_mozi.style.pointerEvents = "auto";
+        Top_mozi.style.color = '#ffffff';
+
+        Popup2.style.visibility = "hidden";
+        News_mozi.style.pointerEvents = "auto";
+        News_mozi.style.color = '#ffffff';
+
+        Popup3.style.visibility = "hidden";
+        Profile_mozi.style.pointerEvents = "auto";
+        Profile_mozi.style.color = '#ffffff';
+
+        People_mozi.style.color = '#e3eb9b';
+        People_mozi.style.pointerEvents = "none";
+
+        Popup5.style.visibility = "hidden";
+        Music_mozi.style.pointerEvents = "auto";
+        Music_mozi.style.color = '#ffffff';
+
+        Popup6.style.visibility = "hidden";
+        Blog_mozi.style.pointerEvents = "auto";
+        Blog_mozi.style.color = '#ffffff';
+
+    } else {
+        Popup4.style.visibility = "hidden";
+        People_mozi.style.color = '#ffffff';
+    }
+
+    Popup4.style.left = "300px";
+    Popup4.style.top = "10px";
+    
+}
+
+
+function OnLinkClick5() {
+    var Popup1 = document.getElementById("PopupFrame1");
+    var Top_mozi = document.getElementById("PopupLink1");
+
+    var Popup2 = document.getElementById("PopupFrame2");
+    var News_mozi = document.getElementById("PopupLink2");
+
+    var Popup3 = document.getElementById("PopupFrame3");
+    var Profile_mozi = document.getElementById("PopupLink3");
+
+    var Popup4 = document.getElementById("PopupFrame4");
+    var People_mozi = document.getElementById("PopupLink4");
+
+    var Popup5 = document.getElementById("PopupFrame5");
+    var Music_mozi = document.getElementById("PopupLink5");
+
+    var Popup6 = document.getElementById("PopupFrame6");
+    var Blog_mozi = document.getElementById("PopupLink6");
+
+    Music_mozi.style.color = '#a0d3f7';
+    
+    if (Popup5.style.visibility === "hidden") {
+        Popup5.style.visibility = "visible";
+
+        Popup1.style.visibility = "hidden";
+        Top_mozi.style.pointerEvents = "auto";
+        Top_mozi.style.color = '#ffffff';
+
+        Popup2.style.visibility = "hidden";
+        News_mozi.style.pointerEvents = "auto";
+        News_mozi.style.color = '#ffffff';
+
+        Popup3.style.visibility = "hidden";
+        Profile_mozi.style.pointerEvents = "auto";
+        Profile_mozi.style.color = '#ffffff';
+
+        Popup4.style.visibility = "hidden";
+        People_mozi.style.pointerEvents = "auto";
+        People_mozi.style.color = '#ffffff';
+
+        Music_mozi.style.color = '#e3eb9b';
+        Music_mozi.style.pointerEvents = "none";
+
+        Popup6.style.visibility = "hidden";
+        Blog_mozi.style.pointerEvents = "auto";
+        Blog_mozi.style.color = '#ffffff';
+
+    } else {
+        Popup5.style.visibility = "hidden";
+        Music_mozi.style.color = '#ffffff';
+    }
+
+    Popup5.style.left = "300px";
+    Popup5.style.top = "10px";
+    
+}
+
+
+
+function OnLinkClick6() {
+    var Popup1 = document.getElementById("PopupFrame1");
+    var Top_mozi = document.getElementById("PopupLink1");
+
+    var Popup2 = document.getElementById("PopupFrame2");
+    var News_mozi = document.getElementById("PopupLink2");
+
+    var Popup3 = document.getElementById("PopupFrame3");
+    var Profile_mozi = document.getElementById("PopupLink3");
+
+    var Popup4 = document.getElementById("PopupFrame4");
+    var People_mozi = document.getElementById("PopupLink4");
+
+    var Popup5 = document.getElementById("PopupFrame5");
+    var Music_mozi = document.getElementById("PopupLink5");
+
+    var Popup6 = document.getElementById("PopupFrame6");
+    var Blog_mozi = document.getElementById("PopupLink6");
+
+    Blog_mozi.style.color = '#a0d3f7';
+    
+    if (Popup6.style.visibility === "hidden") {
+        Popup6.style.visibility = "visible";
+
+        Popup1.style.visibility = "hidden";
+        Top_mozi.style.pointerEvents = "auto";
+        Top_mozi.style.color = '#ffffff';
+
+        Popup2.style.visibility = "hidden";
+        News_mozi.style.pointerEvents = "auto";
+        News_mozi.style.color = '#ffffff';
+
+        Popup3.style.visibility = "hidden";
+        Profile_mozi.style.pointerEvents = "auto";
+        Profile_mozi.style.color = '#ffffff';
+
+        Popup4.style.visibility = "hidden";
+        People_mozi.style.pointerEvents = "auto";
+        People_mozi.style.color = '#ffffff';
+
+        Popup5.style.visibility = "hidden";
+        Music_mozi.style.pointerEvents = "auto";
+        Music_mozi.style.color = '#ffffff';
+
+        Blog_mozi.style.color = '#e3eb9b';
+        Blog_mozi.style.pointerEvents = "none";
+
+    } else {
+        Popup6.style.visibility = "hidden";
+        Blog_mozi.style.color = '#ffffff';
+    }
+
+    Popup6.style.left = "300px";
+    Popup6.style.top = "10px";
+    
+}
